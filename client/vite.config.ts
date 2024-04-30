9ea77e2b4dcd4b2bdb0f6239228a95d226dd5336
liyp.86 <liyp.86@bytedance.com>
2024-04-30 15:00:33 +0800

    feat: init

---
+++ client/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 8000, // 指定 dev server 的端口号，默认是 3000
+    open: true, // 启动 dev server 时是否自动打开浏览器
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '/api'),
+      },
+    },
+  },
+})
